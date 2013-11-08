0b584eee7a579f657d86f8537ef9cd0374c1de06
James Manning <james.manning@gmail.com>
2013-11-08 02:53:39 -0500

    make datagridview columns sortable
    - this uses the SortableBindingList<T> class from
      http://betimvwframework.codeplex.com/ (Apache 2.0 license)
      to wrap the existing List<ProjectModel> and allow sorting

---
+++ TargetFrameworkMigrator/SortableBindingList.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSChangeTargetFrameworkExtension
+{
+    /// <summary>
+    ///  List wrapper class to provide basic sorting operations when bound to a DataGrid view.  
+    ///  From the Apache 2.0 licensed project http://betimvwframework.codeplex.com/
+    ///  Small changes to make it easier to use in the TargetFrameworkMigrator project.
+    /// </summary>
+    public class SortableBindingList<T> : BindingList<T>
+    {
+        private List<T> wrappedList;
+        private readonly Dictionary<Type, PropertyComparer<T>> comparers;
+        private bool isSorted;
+        private ListSortDirection listSortDirection;
+        private PropertyDescriptor propertyDescriptor;
+
+        public SortableBindingList(List<T> wrappedList)
+            : base(wrappedList)
+        {
+            this.wrappedList = wrappedList;
+            this.comparers = new Dictionary<Type, PropertyComparer<T>>();
+        }
+
+        public List<T> WrappedList
+        {
+            get { return wrappedList; }
+        }
+
+        protected override bool SupportsSortingCore
+        {
+            get { return true; }
+        }
+
+        protected override bool IsSortedCore
+        {
+            get { return this.isSorted; }
+        }
+
+        protected override PropertyDescriptor SortPropertyCore
+        {
+            get { return this.propertyDescriptor; }
+        }
+
+        protected override ListSortDirection SortDirectionCore
+        {
+            get { return this.listSortDirection; }
+        }
+
+        protected override bool SupportsSearchingCore
+        {
+            get { return true; }
+        }
+
+        protected override void ApplySortCore(PropertyDescriptor property, ListSortDirection direction)
+        {
+            List<T> itemsList = (List<T>)this.Items;
+
+            Type propertyType = property.PropertyType;
+            PropertyComparer<T> comparer;
+            if (!this.comparers.TryGetValue(propertyType, out comparer))
+            {
+                comparer = new PropertyComparer<T>(property, direction);
+                this.comparers.Add(propertyType, comparer);
+            }
+
+            comparer.SetPropertyAndDirection(property, direction);
+            itemsList.Sort(comparer);
+
+            this.propertyDescriptor = property;
+            this.listSortDirection = direction;
+            this.isSorted = true;
+
+            this.OnListChanged(new ListChangedEventArgs(ListChangedType.Reset, -1));
+        }
+
+        protected override void RemoveSortCore()
+        {
+            this.isSorted = false;
+            this.propertyDescriptor = base.SortPropertyCore;
+            this.listSortDirection = base.SortDirectionCore;
+
+            this.OnListChanged(new ListChangedEventArgs(ListChangedType.Reset, -1));
+        }
+
+        protected override int FindCore(PropertyDescriptor property, object key)
+        {
+            int count = this.Count;
+            for (int i = 0; i < count; ++i)
+            {
+                T element = this[i];
+                if (property.GetValue(element).Equals(key))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    /// <summary>
+    /// helper class for supporting comparison operations in SortableBindingList
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PropertyComparer<T> : IComparer<T>
+    {
+        private readonly IComparer comparer;
+        private PropertyDescriptor propertyDescriptor;
+        private int reverse;
+
+        public PropertyComparer(PropertyDescriptor property, ListSortDirection direction)
+        {
+            this.propertyDescriptor = property;
+            Type comparerForPropertyType = typeof(Comparer<>).MakeGenericType(property.PropertyType);
+            this.comparer = (IComparer)comparerForPropertyType.InvokeMember("Default", BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.Public, null, null, null);
+            this.SetListSortDirection(direction);
+        }
+
+        #region IComparer<T> Members
+
+        public int Compare(T x, T y)
+        {
+            return this.reverse * this.comparer.Compare(this.propertyDescriptor.GetValue(x), this.propertyDescriptor.GetValue(y));
+        }
+
+        #endregion
+
+        private void SetPropertyDescriptor(PropertyDescriptor descriptor)
+        {
+            this.propertyDescriptor = descriptor;
+        }
+
+        private void SetListSortDirection(ListSortDirection direction)
+        {
+            this.reverse = direction == ListSortDirection.Ascending ? 1 : -1;
+        }
+
+        public void SetPropertyAndDirection(PropertyDescriptor descriptor, ListSortDirection direction)
+        {
+            this.SetPropertyDescriptor(descriptor);
+            this.SetListSortDirection(direction);
+        }
+    }
+}
