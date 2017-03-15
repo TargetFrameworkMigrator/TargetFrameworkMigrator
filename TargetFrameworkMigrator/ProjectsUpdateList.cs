6571a3e18d9e76da7c47c024cfc2b8fea205f652
Wolfgang Stelzhammer <stelzhammer.w@live.com>
2017-03-15 22:19:45 +0100

    fixed compiletiem errors and warnings

--- TargetFrameworkMigrator/ProjectsUpdateList.cs
+++ TargetFrameworkMigrator/ProjectsUpdateList.cs
@@ -4,143 +4,184 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using EnvDTE;
 using VSChangeTargetFrameworkExtension.Annotations;
 
 namespace VSChangeTargetFrameworkExtension
 {
-    public partial class ProjectsUpdateList : Form
+  public partial class ProjectsUpdateList : Form
+  {
+    public event Action UpdateFired;
+    public event Action ReloadFired;
+
+    public ProjectsUpdateList()
     {
-        public event Action UpdateFired;
-        public event Action ReloadFired;
-
-        public ProjectsUpdateList()
-        {
-            InitializeComponent();
-            dataGridView1.AutoGenerateColumns = false;
-            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
-        }
-
-        public List<FrameworkModel> Frameworks { set { comboBox1.DataSource = value; } }
-        public List<ProjectModel> Projects
-        {
-            set
-            {
-                var wrapperBindingList = new SortableBindingList<ProjectModel>(value);
-                dataGridView1.DataSource = wrapperBindingList;
-                dataGridView1.Refresh();
-            }
-            get
-            {
-                var wrapperBindingList = (SortableBindingList<ProjectModel>)dataGridView1.DataSource;
-                return wrapperBindingList.WrappedList;
-            }
-        }
-
-        public FrameworkModel SelectedFramework
-        {
-            get { return (FrameworkModel) comboBox1.SelectedItem; }
-        }
-
-        public string State { set { label1.Text = value; } }
-
-        private async void button3_Click(object sender, EventArgs e)
-        {
-            var onUpdate = UpdateFired;
-            if(onUpdate != null)
-                onUpdate.Invoke();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            foreach (var projectModel in Projects)
-            {
-                projectModel.IsSelected = true;
-            }
-            dataGridView1.Refresh();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            foreach (var projectModel in Projects)
-            {
-                projectModel.IsSelected = false;
-            }
-            dataGridView1.Refresh();
-        }
-
-        private void reloadButton_Click(object sender, EventArgs e)
-        {
-            var onReloadFired = ReloadFired;
-            if(onReloadFired != null)
-                onReloadFired.Invoke();
-        }
+      InitializeComponent();
+      dataGridView1.AutoGenerateColumns = false;
+      comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
     }
 
-    public class ProjectModel:INotifyPropertyChanged
+    public List<FrameworkModel> Frameworks
     {
-        private bool isSelected;
-        private string name;
-
-        public bool IsSelected
-        {
-            get { return isSelected; }
-            set
-            {
-                isSelected = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set
-            {
-                name = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public FrameworkModel Framework { get; set; }
-
-        public bool HasFramework
-        {
-            get { return Framework != null; }
-        }
-
-        public Project DteProject { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("{0}", Name);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        }
+      set
+      {
+        comboBox1.DataSource = value;
+      }
+    }
+    public List<ProjectModel> Projects
+    {
+      set
+      {
+        var wrapperBindingList = new SortableBindingList<ProjectModel>(value);
+        dataGridView1.DataSource = wrapperBindingList;
+        dataGridView1.Refresh();
+      }
+      get
+      {
+        var wrapperBindingList = (SortableBindingList<ProjectModel>)dataGridView1.DataSource;
+        return wrapperBindingList.WrappedList;
+      }
     }
 
-    public class FrameworkModel : IComparable
+    public FrameworkModel SelectedFramework
     {
-        public string Name { get; set; }
-        public uint Id { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("{0}", Name);
-        }
-
-        // support comparison so we can sort by properties of this type
-        public int CompareTo(object obj)
-        {
-            return StringComparer.Ordinal.Compare(this.Name, ((FrameworkModel)obj).Name);
-        }
+      get
+      {
+        return (FrameworkModel)comboBox1.SelectedItem;
+      }
     }
+
+    public string State
+    {
+      set
+      {
+        label1.Text = value;
+      }
+    }
+
+    private async void button3_Click(object sender, EventArgs e)
+    {
+      var onUpdate = UpdateFired;
+      if (onUpdate != null)
+        await Task.Run(() =>
+        {
+          onUpdate.Invoke();
+        });
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      foreach (var projectModel in Projects)
+      {
+        projectModel.IsSelected = true;
+      }
+      dataGridView1.Refresh();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      foreach (var projectModel in Projects)
+      {
+        projectModel.IsSelected = false;
+      }
+      dataGridView1.Refresh();
+    }
+
+    private void reloadButton_Click(object sender, EventArgs e)
+    {
+      var onReloadFired = ReloadFired;
+      if (onReloadFired != null)
+        onReloadFired.Invoke();
+    }
+  }
+
+  public class ProjectModel : INotifyPropertyChanged
+  {
+    private bool isSelected;
+    private string name;
+
+    public bool IsSelected
+    {
+      get
+      {
+        return isSelected;
+      }
+      set
+      {
+        isSelected = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public string Name
+    {
+      get
+      {
+        return name;
+      }
+      set
+      {
+        name = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public FrameworkModel Framework
+    {
+      get; set;
+    }
+
+    public bool HasFramework
+    {
+      get
+      {
+        return Framework != null;
+      }
+    }
+
+    public Project DteProject
+    {
+      get; set;
+    }
+
+    public override string ToString()
+    {
+      return string.Format("{0}", Name);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+      var handler = PropertyChanged;
+      if (handler != null)
+        handler(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+
+  public class FrameworkModel : IComparable
+  {
+    public string Name
+    {
+      get; set;
+    }
+    public uint Id
+    {
+      get; set;
+    }
+
+    public override string ToString()
+    {
+      return string.Format("{0}", Name);
+    }
+
+    // support comparison so we can sort by properties of this type
+    public int CompareTo(object obj)
+    {
+      return StringComparer.Ordinal.Compare(this.Name, ((FrameworkModel)obj).Name);
+    }
+  }
 }
